ab1317717c7e924a58853b09d0e25711d8a8b5e2
Bruno Montalto <brunomontaltoglok@gmail.com>
2022-05-11 18:41:41 +0200

    binary converter

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,33 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include<iostream>
+#include<string>
+
+using namespace std;
+string toBinary(int n){
+	string res = "";
+	while (n){
+		if(n%2 == 0){
+			res += '0';
+		}
+		else {
+			res += '1';
+		}
+		
+		n >>= 1;
+	}
+	for (int i = 0; i < res.length() / 2; i++)
+        swap(res[i], res[res.length() - i - 1]);
+	return res;
+}
+
+int main(){
+	int n;
+	cout << "Insert first number: ";
+	cin >> n;
+	cout << "The binary number is: " << toBinary(n) << endl;
+	
+	
+	return 0;
+}
